70a19e604f5e54688981624672d14d0ca5ab99a2
dourl <1141851728@qq.com>
2018-07-06 10:11:14 +0800

    在dev 分支上 做了一些开发

--- app/src/main/java/top/douruanliang/fragmentlazyapp/MainActivity.java
+++ app/src/main/java/top/douruanliang/fragmentlazyapp/MainActivity.java
@@ -2,6 +2,8 @@ package top.douruanliang.fragmentlazyapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -21,14 +23,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
+        });
 
 
         mBtn = findViewById(R.id.mbtn_id);
